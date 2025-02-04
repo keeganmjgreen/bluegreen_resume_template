4dcd10ed9d50e8ddff8e42a12cf9f16bd5e776b3
Keegan Green <keegan.green@bluwave-ai.com>
2025-02-04 11:32:40 -0500

    pydantic model `to_html` methods

--- yml_to_resume.py
+++ yml_to_resume.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import abc
 import argparse
 from pathlib import Path
+from typing import Literal
 import pydantic
 import yaml
 
@@ -14,23 +16,78 @@ args = parser.parse_args()
 pages = [int(page) for page in args.pages.split(",")]
 
 
-class ResumeSection(pydantic.BaseModel):
+class ResumeSection(pydantic.BaseModel, abc.ABC):
     title: str
+
+    @abc.abstractmethod
+    def to_html(self) -> str:
+        raise NotImplementedError
 
 
 class SkillsResumeSection(ResumeSection):
     content: dict[str, list[str]]
 
+    def to_html(self) -> str:
+        return f'<div class="heading left-heading">{self.title}</div>' + "".join(
+            [
+                f'<div class="entry-heading"><div class="left">{skill_group_name}</div></div>'
+                + '<ul class="padded-list">'
+                + "".join([f"<li>{skill}</li>" for skill in skills])
+                + "</ul>"
+                for skill_group_name, skills in self.content.items()
+            ]
+        )
+
 
 class HighlightsResumeSection(ResumeSection):
     content: list[str]
 
-
-SingleKeyDict = dict
+    def to_html(self) -> str:
+        return (
+            f'<div class="heading right-heading">{self.title}</div>'
+            + "<ul>"
+            + "".join([f"<li>{highlight}</li>" for highlight in self.content])
+            + "</ul>"
+        )
 
 
 class ResumeSectionWithEntries(ResumeSection):
     content: list[ResumeEntry]
+    outer_list_css_class: Literal["padded-list", "unpadded-list"] = "unpadded-list"
+
+    def to_html(self) -> str:
+        return f'<div class="heading right-heading">{self.title}</div>' + "".join(
+            [
+                f'<div class="entry-heading"><div class="left">{entry.name}</div><div class="right">{entry.period}</div></div>'
+                + (
+                    f'<div class="entry-subheading"><div class="left">{entry.institution}</div><div class="right">{entry.location}</div></div>'
+                    if entry.institution or entry.location
+                    else ""
+                )
+                + f'<ul class="{self.outer_list_css_class}">'
+                + "".join(
+                    [
+                        f"<li>{x if type(x) is str else list(x.keys())[0] + f'<ul class="unpadded-list">{"".join([f"<li>{v}</li>" for v in list(x.values())[0]])}</ul>'}</li>"
+                        for x in entry.content
+                    ]
+                )
+                + "</ul>"
+                for entry in self.content
+            ]
+        )
+
+
+class ExperiencesSection(ResumeSectionWithEntries):
+    title: str = "Experience"
+    outer_list_css_class: Literal["padded-list"] = "padded-list"
+
+
+class QualificationsSection(ResumeSectionWithEntries):
+    title: str = "Education"
+
+
+class ProjectsSection(ResumeSectionWithEntries):
+    title: str = "Projects"
 
 
 class ResumeEntry(pydantic.BaseModel):
@@ -39,6 +96,9 @@ class ResumeEntry(pydantic.BaseModel):
     institution: str = ""
     location: str = ""
     content: list[str | SingleKeyDict[str, list[str]]]
+
+
+SingleKeyDict = dict
 
 
 yml = yaml.safe_load(Path(args.resume_yml).read_text())
@@ -52,57 +112,21 @@ for page in pages:
     sections |= yml[f"page_{page}"]
 sidebar_output = ""
 if sections.get("skills_section") is not None:
-    section = SkillsResumeSection(**sections["skills_section"])
-    sidebar_output += (
-        f'<div class="heading left-heading">{section.title}</div>'
-        + "".join(
-            [
-                f'<div class="entry-heading"><div class="left">{skill_group}</div></div>'
-                + '<ul class="padded-list">'
-                + "".join([f"<li>{skill}</li>" for skill in skills])
-                + "</ul>"
-                for skill_group, skills in section.content.items()
-            ]
-        )
-    )
+    sidebar_output += SkillsResumeSection(**sections["skills_section"]).to_html()
 template = template.replace("<!-- sidebar -->", sidebar_output)
 main_output = ""
 if sections.get("highlights_section") is not None:
-    section = HighlightsResumeSection(**sections["highlights_section"])
-    main_output += (
-        f'<div class="heading right-heading">{section.title}</div>'
-        + "<ul>"
-        + "".join([f"<li>{highlight}</li>" for highlight in section.content])
-        + "</ul>"
-    )
+    main_output += HighlightsResumeSection(**sections["highlights_section"]).to_html()
 for section_name in [
     "experiences_section",
     "qualifications_section",
     "projects_section",
 ]:
     if sections.get(section_name) is not None:
-        section = ResumeSectionWithEntries(**sections[section_name])
-        main_output += (
-            f'<div class="heading right-heading">{section.title}</div>'
-            + "".join(
-                [
-                    f'<div class="entry-heading"><div class="left">{entry.name}</div><div class="right">{entry.period}</div></div>'
-                    + (
-                        f'<div class="entry-subheading"><div class="left">{entry.institution}</div><div class="right">{entry.location}</div></div>'
-                        if entry.institution or entry.location
-                        else ""
-                    )
-                    + f'<ul class="{"padded" if section_name == "experiences_section" else "unpadded"}-list">'
-                    + "".join(
-                        [
-                            f"<li>{x if type(x) is str else list(x.keys())[0] + f'<ul class="unpadded-list">{"".join([f"<li>{v}</li>" for v in list(x.values())[0]])}</ul>'}</li>"
-                            for x in entry.content
-                        ]
-                    )
-                    + "</ul>"
-                    for entry in section.content
-                ]
-            )
-        )
+        main_output += {
+            "experiences_section": ExperiencesSection,
+            "qualifications_section": QualificationsSection,
+            "projects_section": ProjectsSection,
+        }[section_name](**sections[section_name]).to_html()
 template = template.replace("<!-- main -->", main_output)
 Path(args.output).write_text(template)
