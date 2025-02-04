b162c5966d38129f0e304107a24c759001cdeb35
Keegan Green <keegan.green@bluwave-ai.com>
2025-02-04 11:45:18 -0500

    further code improvements

--- yml_to_resume.py
+++ yml_to_resume.py
@@ -2,18 +2,12 @@ from __future__ import annotations
 
 import abc
 import argparse
+from copy import deepcopy
 from pathlib import Path
 from typing import ClassVar, Literal
+
 import pydantic
 import yaml
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--pages", default="1,2")
-parser.add_argument("--template", default="resume_template.html")
-parser.add_argument("--resume-yml", default="bluegreen_resume.yml")
-parser.add_argument("--output", default="resume.html")
-args = parser.parse_args()
-page_numbers = [int(page) for page in args.pages.split(",")]
 
 
 class ResumeSection(pydantic.BaseModel, abc.ABC):
@@ -128,22 +122,45 @@ SECTION_CLASSES_BY_REGION: dict[Region, list[ResumeSection]] = {
     ],
 }
 
-yml = yaml.safe_load(Path(args.resume_yml).read_text())
-template = Path(args.template).read_text()
-for field in ["name", "tagline", "phone", "email", "location"]:
-    template = template.replace(f"<!-- {field} -->", yml[field])
-sections_yml = {}
-for page_number in page_numbers:
-    sections_yml |= yml[f"page_{page_number}"]
-for region, section_classes in SECTION_CLASSES_BY_REGION.items():
-    template = template.replace(
-        f"<!-- {region} -->",
-        "".join(
-            [
-                section_class(**sections_yml[section_class.name]).to_html()
-                for section_class in section_classes
-                if sections_yml.get(section_class.name) is not None
-            ]
-        ),
+
+def parse_cli_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--template", default="resume_template.html")
+    parser.add_argument("--resume-yml", default="bluegreen_resume.yml")
+    parser.add_argument("--page-number", type=int, default=None)
+    parser.add_argument("--output", default="resume.html")
+    return parser.parse_args()
+
+
+def convert_resume_yml_to_html(
+    template: str, resume_yml: str, page_number: int | None = None
+) -> str:
+    page_numbers = [1, 2] if page_number is None else [page_number]
+    template = deepcopy(template)
+    for field in ["name", "tagline", "phone", "email", "location"]:
+        template = template.replace(f"<!-- {field} -->", resume_yml[field])
+    sections_yml = {}
+    for page_number in page_numbers:
+        sections_yml |= resume_yml[f"page_{page_number}"]
+    for region, section_classes in SECTION_CLASSES_BY_REGION.items():
+        template = template.replace(
+            f"<!-- {region} -->",
+            "".join(
+                [
+                    section_class(**sections_yml[section_class.name]).to_html()
+                    for section_class in section_classes
+                    if sections_yml.get(section_class.name) is not None
+                ]
+            ),
+        )
+    return template
+
+
+if __name__ == "__main__":
+    args = parse_cli_args()
+    html = convert_resume_yml_to_html(
+        template=Path(args.template).read_text(),
+        resume_yml=yaml.safe_load(Path(args.resume_yml).read_text()),
+        page_number=args.page_number,
     )
-Path(args.output).write_text(template)
+    Path(args.output).write_text(html)
