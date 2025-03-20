ac36c6dcae5c5541ecf08157141e7398e932d0b3
Keegan Green <keegan.green@bluwave-ai.com>
2025-03-20 16:59:55 -0400

    settings specific to `green-resume` branch

--- build_resume.sh
+++ build_resume.sh
@@ -1,4 +1,4 @@
-python yml_to_resume.py --output=resume.html
+python yml_to_resume.py --output=green_resume.html
 
 python yml_to_resume.py --page-number=1 --output=resume_page_1.html
 google-chrome resume_page_1.html --headless --print-to-pdf=resume_page_1.pdf --no-pdf-header-footer
@@ -8,7 +8,7 @@ python yml_to_resume.py --page-number=2 --output=resume_page_2.html
 google-chrome resume_page_2.html --headless --print-to-pdf=resume_page_2.pdf --no-pdf-header-footer
 rm resume_page_2.html
 
-pdfunite resume_page_1.pdf resume_page_2.pdf resume.pdf
+pdfunite resume_page_1.pdf resume_page_2.pdf green_resume.pdf
 
 rm resume_page_1.pdf
 rm resume_page_2.pdf
