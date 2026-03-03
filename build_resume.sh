0589852c2007073fff644a930ace5f152880aae9
Keegan Green <keeganmjgreen@gmail.com>
2026-03-02 22:33:42 -0500

    fix: ensure populated subtemplate not overwritten by later `yml_to_resume.py` invocations in `build_resume.sh`

--- build_resume.sh
+++ build_resume.sh
@@ -1,5 +1,3 @@
-uv run yml_to_resume.py --output=resume.html
-
 uv run yml_to_resume.py --page-number=1 --output=resume_page_1.html
 google-chrome resume_page_1.html --headless --print-to-pdf=resume_page_1.pdf --no-pdf-header-footer
 rm resume_page_1.html
@@ -12,3 +10,5 @@ pdfunite resume_page_1.pdf resume_page_2.pdf resume.pdf
 
 rm resume_page_1.pdf
 rm resume_page_2.pdf
+
+uv run yml_to_resume.py --output=resume.html
